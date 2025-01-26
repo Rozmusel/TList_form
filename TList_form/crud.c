2bc0b3377f37b11034593be2247314e5adceff2e
Ivan <100085476+Rozmusel@users.noreply.github.com>
2025-01-26 20:41:42 +0300

    Commands demo

--- TList_form/crud.c
+++ TList_form/crud.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 int get_CRUD(char* input) {
-	char* com[] = {"create ", "read ", "update ", "delete ", "import ", "export ", "help "};
+	char* com[] = {"create", "read", "update", "delete", "import", "export", "help"};
 	for (int i = 0; i < 6; i++){
 		if(strncmp(input, com[i], strlen(com[i])) == 0)
 			return ++i;
